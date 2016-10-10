5b7bae76bc08bacae09e005eb16d40094f8a91ab
Tina Stith-Twine <tinatwine@gmail.com>
2016-10-10 00:24:14 -0400

    Poll Display

---
+++ app.py
@@ -0,0 +1,15 @@
+import web
+
+urls = ('/', 'Index')
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/')
+
+class Index(object):
+	def GET(self):
+		greeting = "Hello World"
+return render.index(greeting = greeting)
+if_name__ == "__main__":app.run()
+
+
